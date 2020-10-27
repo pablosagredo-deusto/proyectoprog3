7f88a19afd6b1939f17170db296b55f23e8ffea3
Guillermo Perez <guillermo.perez@opendeusto.es>
2020-10-28 00:08:18 +0100

    prueba

---
+++ FoodEst/src/foodEst/VentanaPrincipal.java
@@ -0,0 +1,15 @@
+package foodEst;
+
+import javax.swing.JFrame;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Calendar;
+import java.util.Date;
+import javax.swing.*;
+
+public class VentanaPrincipal extends JFrame {
+	
+	public VentanaPrincipal() {
+		JButton supermercado =  new J
+	}
+}
