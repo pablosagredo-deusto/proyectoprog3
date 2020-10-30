735f7d223fff766d1a31f6d91a1da30b2a00c5db
Pablo <pablo.sagredo@opendeusto.es>
2020-10-30 17:48:09 +0100

    commit prueba

--- FoodEst/src/clases/Ingrediente.java
+++ FoodEst/src/clases/Ingrediente.java
@@ -2,6 +2,46 @@ package clases;
 
 public class Ingrediente {
 	String nombre;
+	String id;
+	String tipo;
+
+	public Ingrediente(String nombre, String id, String tipo) {
+		super();
+		this.nombre = nombre;
+		this.id = id;
+		this.tipo = tipo;
+	}
+
+	public Ingrediente() {
+		super();
+		this.nombre = "";
+		this.id = "";
+		this.tipo = "";
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
 	
 
 }
