d121158cb6d7cc3ace9b8b224a2ad82ba5096e40
Eduardo Rocha <eduardo.rocha@opendeusto.es>
2020-10-31 14:33:31 +0100

    Ventana y Registro base

--- FoodEst/src/ventanas/VentanaLogin.java
+++ FoodEst/src/ventanas/VentanaLogin.java
@@ -1,28 +1,111 @@
 package ventanas;
 
-import java.awt.*;
 
-import javax.swing.*;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.font.TextAttribute;
+import java.util.Map;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class VentanaLogin extends JFrame {
-
+	
+	JPanel pnlCentral;
+	JTextField usuario;
+	JTextField contraseña;
+	JLabel mail;
+	JLabel llave;
+	JLabel recuperar;
+	JLabel registrar;
+	JLabel titulo;
+	JButton user;
+	JButton restaurante;
+	
+	
+	
 	public VentanaLogin() {
-
-		JPanel panelAbajo = new JPanel();
-		JButton supermercado = new JButton("Supermercados");
-		JButton enviosExpres = new JButton("Envios expres");
-		JButton tiendas = new JButton("Tiendas");
-		JButton comida = new JButton("Comida");
-		JButton donacion = new JButton("Donacion");
-		JPanel panelBotones = new JPanel();
-		panelBotones.setLayout(new GridLayout());
-
-		panelAbajo.setLayout(new GridLayout(2, 1));
-
-	}
-
-	public static void main(String[] args) {
 		
-
+		setTitle("Login");
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setResizable(false);
+		setSize(550, 700);
+		setLocationRelativeTo(null);
+		getContentPane().setLayout(new BorderLayout(0, 0));
+		pnlCentral = new JPanel();
+		getContentPane().add(pnlCentral, BorderLayout.CENTER);
+		pnlCentral.setLayout(null);
+		pnlCentral.setBackground(Color.white);
+		
+		//Textfields
+		
+		usuario = new JTextField();
+		usuario.setBounds(250, 180, 180, 40);
+		pnlCentral.add(usuario);
+		
+		contraseña = new JTextField();
+		contraseña.setBounds(250, 280, 180, 40);
+		pnlCentral.add(contraseña);
+		
+		//Labels
+		
+		llave = new JLabel();
+		llave.setIcon(new ImageIcon("key.png"));
+		llave.setBounds(110, 170, 120, 60);
+		pnlCentral.add(llave);
+		
+		mail = new JLabel();
+		mail.setIcon(new ImageIcon("mail.png"));
+		mail.setBounds(130, 270, 120, 60);
+		pnlCentral.add(mail);
+		
+		titulo = new JLabel();
+		titulo.setText("Inicia sesión en tu cuenta Foodest");
+		titulo.setBounds(50, 30, 450, 90);
+		titulo.setFont(new Font("Arial", Font.PLAIN, 30));
+		pnlCentral.add(titulo);
+		
+		recuperar = new JLabel();
+		recuperar.setText("Recuperar contraseña");
+		recuperar.setForeground(Color.blue);
+		recuperar.setBounds(198, 330, 200, 90);
+		recuperar.setFont(new Font("Arial", Font.PLAIN, 15)); 
+		pnlCentral.add(recuperar);
+		
+		
+		registrar = new JLabel();
+		registrar.setText("¿No tienes cuenta? Registrate");
+		registrar.setForeground(Color.blue);
+		registrar.setBounds(175, 540, 200, 60);
+		registrar.setFont(new Font("Arial", Font.PLAIN, 15)); 
+		pnlCentral.add(registrar);
+		
+		//Botones
+		
+		
+		restaurante = new JButton();
+		restaurante.setText("Restaurante");
+		restaurante.setBackground(Color.white);
+		restaurante.setFont(new Font("Arial", Font.PLAIN, 22));
+		restaurante.setBounds(269, 430, 190, 90);
+		pnlCentral.add(restaurante);
+		
+		user = new JButton();
+		user.setText("Usuario");
+		user.setBackground(Color.LIGHT_GRAY);
+		user.setFont(new Font("Arial", Font.PLAIN, 22));
+		user.setBounds(75, 430, 190, 90);
+		pnlCentral.add(user);
+		
+		setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		VentanaLogin log = new VentanaLogin();
 	}
 }
---
+++ FoodEst/src/ventanas/VentanaRegistro.java
@@ -0,0 +1,101 @@
+package ventanas;
+
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class VentanaRegistro extends JFrame{
+	
+	JPanel pnlCentral;
+	JTextField correo;
+	JTextField usuario;
+	JTextField contraseña;
+	JButton restaurante;
+	JButton user;
+	JLabel mail;
+	JLabel llave;
+	JLabel titulo;
+	JLabel userIcon;
+	
+	public VentanaRegistro() {
+		
+		setTitle("Registro");
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setResizable(false);
+		setSize(1150, 505);
+		setLocationRelativeTo(null);
+		getContentPane().setLayout(new BorderLayout(0, 0));
+		pnlCentral = new JPanel();
+		getContentPane().add(pnlCentral, BorderLayout.CENTER);
+		pnlCentral.setLayout(null);
+		pnlCentral.setBackground(Color.white);
+		
+		
+		
+		//Textfields
+		
+		usuario = new JTextField();
+		usuario.setBounds(250, 190, 180, 40);
+		pnlCentral.add(usuario);
+				
+		contraseña = new JTextField();
+		contraseña.setBounds(250, 290, 180, 40);
+		pnlCentral.add(contraseña);
+		
+		correo = new JTextField();
+		correo.setBounds(250, 390, 180, 40);
+		pnlCentral.add(correo);
+		
+		//Botones
+		
+		restaurante = new JButton();
+		restaurante.setText("Restaurante");
+		restaurante.setBackground(Color.white);
+		restaurante.setFont(new Font("Arial", Font.PLAIN, 22));
+		restaurante.setBounds(269, 490, 190, 90);
+		pnlCentral.add(restaurante);
+		
+		user = new JButton();
+		user.setText("Usuario");
+		user.setBackground(Color.LIGHT_GRAY);
+		user.setFont(new Font("Arial", Font.PLAIN, 22));
+		user.setBounds(75, 490, 190, 90);
+		pnlCentral.add(user);
+		
+		//Labels
+		
+		llave = new JLabel();
+		llave.setIcon(new ImageIcon("key.png"));
+		llave.setBounds(110, 270, 120, 60);
+		pnlCentral.add(llave);
+				
+		mail = new JLabel();
+		mail.setIcon(new ImageIcon("mail.png"));
+		mail.setBounds(130, 370, 120, 60);
+		pnlCentral.add(mail);	
+		
+		userIcon = new JLabel();
+		userIcon.setIcon(new ImageIcon("user.png"));
+		userIcon.setBounds(130, 170, 120, 70);
+		pnlCentral.add(userIcon);	
+		
+		titulo = new JLabel();
+		titulo.setText("Registrate en Foodest");
+		titulo.setBounds(130, 30, 450, 90);
+		titulo.setFont(new Font("Arial", Font.PLAIN, 30));
+		pnlCentral.add(titulo);
+		
+		setVisible(true);
+	}
+	public static void main(String[] args) {
+		VentanaRegistro reg = new VentanaRegistro();
+	}
+}
