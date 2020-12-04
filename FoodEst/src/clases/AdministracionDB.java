46158394ce884f6c41ce9cf9ae1a548a3bfe0ca6
Guillermo Perez <guillermo.perez@opendeusto.es>
2020-12-04 19:30:58 +0100

    commit

--- FoodEst/src/clases/AdministracionDB.java
+++ FoodEst/src/clases/AdministracionDB.java
@@ -3,7 +3,7 @@ package clases;
 public class AdministracionDB {
 	public static void main(String[] args) {
 		try {
-			Class.forName("org.sqlite.JBDC");
+			Class.forName("org.sqlite.JBDC ");
 			
 		} catch ( ClassNotFoundException e) {
 			System.out.println("No se ha podido cargar el driver de la base de datos");
